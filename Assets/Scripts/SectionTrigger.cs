6fb6771fe1443a0f915f16377bb3b54eeb9cba55
Jimmy Saldana <143847881+jimerswagger@users.noreply.github.com>
2025-08-29 10:37:22 -0400

    Add SectionTrigger script

--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -1,16 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SectionTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public GameObject road; //the road prefab, not sure if this prefab will include more than one road
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            Instantiate(road); //make object out of the 'road' prefab
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
