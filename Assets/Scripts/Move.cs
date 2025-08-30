8cad5070d5d227297dabfe8b5b4795807f40f6f4
Jimmy Saldana <143847881+jimerswagger@users.noreply.github.com>
2025-08-29 20:23:06 -0400

    Add Death Barrier for leftover road objects

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,12 +7,20 @@ public class Move : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(0, 0, -2) * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Destroy"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -8,11 +8,11 @@ public class SectionTrigger : MonoBehaviour
 
     public GameObject road; //the road prefab, not sure if this prefab will include more than one road
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Trigger"))
         {
-            Instantiate(road, new Vector3(0, 0, 7.4f), Quaternion.identity); //make object out of the 'road' prefab
+            Instantiate(road, new Vector3(0, 0, 9.5f), Quaternion.identity); //make object out of the 'road' prefab
             //at this new Vector, new position in 3D space, Quaternion.identity here means no rotation.
         }
     }
