1c92fb989f3e5fa90de00c0e896cfa42c6efa80a
Jimmy Saldana <143847881+jimerswagger@users.noreply.github.com>
2025-09-01 16:02:54 -0400

    Increase Road Prefab Length
    The length of the one road currently is 3 road game objects
    
    -The game instantiates them correctly, one instance of the road prefab after the other.
    
    -May need to increase the length of the road prefab more north when adding player movement (acceleration and breaking)

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -13,7 +13,7 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 0, -2) * Time.deltaTime;
+        transform.position += new Vector3(0, 0, -10) * Time.deltaTime;
     }
 
     void OnTriggerEnter(Collider other)
