fea63cae08557aae2e987dc17c6b00cdafe452e0
Jimmy Saldana <143847881+jimerswagger@users.noreply.github.com>
2025-10-13 20:39:16 -0400

    Complete Basic Highway Generation
    - I was finally able to have the highway generate properly at the right position every time.
    
    - Before, it was based on the player's current z position at the time they passed the invisible trigger...+ 44f. Which obviously doesn't work if the player is moving around
    
    - Now, there is a fixed game object attached to the player. And the position of that game object is where the highway will instantiate a new highway.
    
    - Bugs left to work out include instantiating duplicate highways by passing through two invisible barriers at the same time. And having the spawnPos object not be directly on the player at all times.

--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -9,17 +9,22 @@ public class SectionTrigger : MonoBehaviour
 
     public GameObject highway;
 
-    public GameObject spawnNewRoad; //invisible
+    public GameObject spawnNewRoadHere; //invisible box
+
+    void Start()
+    {
+        spawnNewRoadHere = GameObject.FindGameObjectWithTag("Spawn");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Trigger"))
         {
             //if (player moving up, then instantiate at a different z value)
-            Instantiate(highway, new Vector3(0, 0.5f, 48f), Quaternion.identity);
+            Instantiate(highway, spawnNewRoadHere.transform.position, Quaternion.identity);
+            Debug.Log("Spawned at: " + spawnNewRoadHere.transform.position);
             //make an invisible game object that is right at the edge of the road, 
             // so you can instantiate a prefab at the position of that invisible object.
-            // Instantiate(highway, invisibleGameObject.positionVector3, Quaternion.identity);
         }
     }
 
