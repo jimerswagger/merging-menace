ba2989f6c7b5017012f152253dc86bd539889b18
Jimmy Saldana <143847881+jimerswagger@users.noreply.github.com>
2025-10-27 20:52:08 -0400

    Add centered and fixed highway generation
    - Now the highway can generate three lanes in the same center position at all times.
    
    - I ended up keeping a gameobject cube on the player and then attaching another gameobject cube on the 'highway' prefab I instantiate with each pass of the trigger zone.
    
    - The cube attached to the player is to know the Z-position of where to spawn the next instance 'highway'. It moves with the player at a fixed distance away. So the generation is not dependent on WHEN the player passes through the trigger.
    
    - However, I realized that I could not rely on the X and Y of the game object cube attached to the player. It works perfectly for the Z position, but its X and Y change with the player.
    
    This is where the gameobject attached to the edge of 'highway' prefab comes in. By using the gameobject's X and Y positions every time a new highway generates, the highway will stay centered. I just make sure there is never more than one of that game object in the scene with an Array with all of that gameobject with that tag in it, and destroying the oldest instance of it, which would be at index 0.

--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.AnimatedValues;
 using UnityEngine;
@@ -10,21 +11,38 @@ public class SectionTrigger : MonoBehaviour
     public GameObject highway;
 
     private GameObject spawnNewRoadHere;
+    private GameObject spawnNewRoadPos;
 
     void Start()
     {
         spawnNewRoadHere = GameObject.FindGameObjectWithTag("Spawn");
+        //spawnNewRoadPos = GameObject.FindGameObjectWithTag("SpawnPos");
+    }
+
+    void Update()
+    {
+        spawnNewRoadPos = GameObject.FindGameObjectWithTag("SpawnPos");
+
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("SpawnPos");
+
+        // Deleting Oldest "SpawnPos" tagged Object
+        if (taggedObjects.Length > 1)
+        {
+            Destroy(taggedObjects[0]);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Trigger"))
         {
-            // Generate Highway at Invisible Game Object
-            Instantiate(highway, spawnNewRoadHere.transform.position, Quaternion.identity);
+            // Generate Highway at Invisible Game Object's X and Y position + Current Z when attached to player
+            Instantiate(highway, new Vector3(spawnNewRoadPos.transform.position.x, spawnNewRoadPos.transform.position.y, spawnNewRoadHere.transform.position.z), Quaternion.identity);
             Debug.Log("Spawned at: " + spawnNewRoadHere.transform.position);
-            
+
         }
     }
+    
+
 
 }
